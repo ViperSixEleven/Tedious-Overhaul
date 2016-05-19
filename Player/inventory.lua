830b82912fb8b2e4238b588c29c3fb2b7948957e
ViperSixEleven <nick.loth@outlook.com>
2016-05-19 12:11:15 +0200

    Update inventory.lua

--- Player/inventory.lua
+++ Player/inventory.lua
@@ -1,6 +1,3 @@
 --Changes the inventory size from 60 to 120
 
-data.raw["player"]["player"].inventory_size = 120
-
---Changes the player health
-data.raw.player.player.max_health=1000
+data.raw["player"]["player"].inventory_size = 20
